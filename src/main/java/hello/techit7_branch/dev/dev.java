249be2d5f1398d23f92c924be5bb28a78b558b6c
param <spsummerdoc@gmail.com>
2024-07-25 15:01:46 +0900

    feat: 김테스트 프린트하는 로직 작성

---
+++ src/main/java/hello/techit7_branch/dev/dev.java
@@ -0,0 +1,8 @@
+package hello.techit7_branch.dev;
+
+public class dev {
+    public static void main(String[] args) {
+        System.out.println("하이하이");
+
+    }
+}
---
+++ src/main/java/hello/techit7_branch/dev/석테스트.java
@@ -0,0 +1,7 @@
+package hello.techit7_branch.dev;
+
+public class 석테스트 {
+    public static void main(String[] args) {
+        System.out.println("김테스트");
+    }
+}
